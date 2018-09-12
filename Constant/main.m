e9cfbe6615a793d9f0d7ec2aa7fa3e508c047eab
Yasser Altamimi <yosy2010\\\\\2gmail.com>
2018-09-12 20:25:51 +0300

    Initial Commit

---
+++ Constant/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Constant
+//
+//  Created by YASSER ALTAMIMI on 02/01/1440 AH.
+//  Copyright © 1440 YASSER ALTAMIMI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
